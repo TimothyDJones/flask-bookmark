cf8e47c8500c93290d6477e20c2c8c8b858308a3
Tim Jones <tdjones74021@yahoo.com>
2020-06-14 16:38:56 -0500

    Add database schema creation/seeding and display of *ALL* bookmarks.

---
+++ app.py
@@ -0,0 +1,17 @@
+# app.py
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def get_db_connection():
+	conn = sqlite3.connect('bookmarks.db')
+	conn.row_factory = sqlite3.Row
+	return conn
+
+@app.route("/")
+def index():
+	conn = get_db_connection()
+	bookmarks = conn.execute('SELECT * FROM bookmarks').fetchall()
+	conn.close()
+	return render_template("index.html", bookmarks=bookmarks)

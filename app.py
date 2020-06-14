231d8eece4a3e921a886cdb8a47d117438f9cbfb
Tim Jones <tdjones74021@yahoo.com>
2020-06-14 17:13:45 -0500

    Add support to create new bookmark via form.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # app.py
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this_is_the_secret_key!'
 
 def get_db_connection():
 	conn = sqlite3.connect('bookmarks.db')
@@ -30,3 +31,24 @@ def index():
 def bookmark(bookmark_id):
 	bookmark = get_bookmark(bookmark_id)
 	return render_template('bookmark.html', bookmark=bookmark)
+
+@app.route("/create", methods=('GET', 'POST'))
+def create():
+	if request.method == 'POST':
+		url = request.form['url']
+		title = request.form['title']
+		description = request.form['description']
+
+		if not url:
+			flash_message('URL is required!')
+		elif  not title:
+			flash_message('Title is required!')
+		else:
+			conn = get_db_connection()
+			conn.execute('INSERT INTO bookmarks (url, title, description) VALUES (?, ?, ?)',
+				(url, title, description))
+			conn.commit()
+			conn.close()
+			return redirect(url_for('index'))
+
+	return render_template('create.html')
